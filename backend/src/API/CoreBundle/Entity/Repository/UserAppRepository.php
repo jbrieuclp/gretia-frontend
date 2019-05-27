498f49cf9f375e49abe716676235ed4638b338db
jbrieuclp <jbrieuclp@gmail.com>
2019-05-27 12:50:47 +0200

    mise à jour vers geonature2db

--- backend/src/API/CoreBundle/Entity/Repository/UserAppRepository.php
+++ backend/src/API/CoreBundle/Entity/Repository/UserAppRepository.php
@@ -26,17 +26,11 @@ class UserAppRepository extends EntityRepository implements UserLoaderInterface
 			$rsm->addFieldResult('u', 'email', 'email');
 			$rsm->addFieldResult('u', 'roles', 'roles');
 
-			$sql = "SELECT roles.id_role, roles.uuid_role, roles.identifiant, roles.nom_role, roles.prenom_role, roles.desc_role, roles.pass, roles.pass_plus, roles.email,
-				array_to_json(array_remove(array_cat(array_agg(DISTINCT app.nom_application)::character varying[], array_agg(DISTINCT concat_ws('_', app.nom_application, ta.tag_code, tob.tag_code))::character varying[]), null)) as roles
-				FROM utilisateurs.t_roles roles
-				LEFT JOIN utilisateurs.cor_roles ON roles.id_role = cor_roles.id_role_utilisateur
-				LEFT JOIN utilisateurs.t_roles groupes ON groupes.id_role = cor_roles.id_role_groupe
-				LEFT JOIN utilisateurs.cor_app_privileges droits ON roles.id_role = droits.id_role OR groupes.id_role = droits.id_role
-				LEFT JOIN utilisateurs.t_tags ta ON droits.id_tag_action = ta.id_tag  
-				LEFT JOIN utilisateurs.t_tags tob ON droits.id_tag_object = tob.id_tag
-				LEFT JOIN utilisateurs.t_applications app ON droits.id_application = app.id_application
+			$sql = "SELECT roles.id_role, roles.identifiant, roles.nom_role, roles.prenom_role, roles.desc_role, roles.pass, roles.pass_plus, roles.email, array_to_json(array_agg(DISTINCT app.code_application)::character varying[]) as roles
+				FROM utilisateurs.v_roleslist_forall_applications roles
+				INNER JOIN utilisateurs.t_applications app ON roles.id_application = app.id_application
 				WHERE roles.identifiant = ?
-				GROUP BY roles.id_role, roles.uuid_role, roles.identifiant, roles.nom_role, roles.prenom_role, roles.desc_role, roles.pass, roles.pass_plus, roles.email";
+				GROUP BY roles.id_role, roles.identifiant, roles.nom_role, roles.prenom_role, roles.desc_role, roles.pass, roles.pass_plus, roles.email";
 
 			$query = $this->_em->createNativeQuery($sql, $rsm);
 			$query->setParameter(1, $username);
@@ -51,7 +45,6 @@ class UserAppRepository extends EntityRepository implements UserLoaderInterface
     	$rsm = new ResultSetMapping;
 			$rsm->addEntityResult('APICoreBundle:UserApp', 'u');
 			$rsm->addFieldResult('u', 'id_role', 'id');
-			$rsm->addFieldResult('u', 'uuid_role', 'uuid');
 			$rsm->addFieldResult('u', 'identifiant', 'username');
 			$rsm->addFieldResult('u', 'nom_role', 'nom');
 			$rsm->addFieldResult('u', 'prenom_role', 'prenom');
@@ -61,17 +54,11 @@ class UserAppRepository extends EntityRepository implements UserLoaderInterface
 			$rsm->addFieldResult('u', 'email', 'email');
 			$rsm->addFieldResult('u', 'roles', 'roles');
 
-			$sql = "SELECT roles.id_role, roles.uuid_role, roles.identifiant, roles.nom_role, roles.prenom_role, roles.desc_role, roles.pass, roles.pass_plus, roles.email,
-				array_to_json(array_remove(array_cat(array_agg(DISTINCT app.nom_application)::character varying[], array_agg(DISTINCT concat_ws('_', app.nom_application, ta.tag_code, tob.tag_code))::character varying[]), null)) as roles
-				FROM utilisateurs.t_roles roles
-				LEFT JOIN utilisateurs.cor_roles ON roles.id_role = cor_roles.id_role_utilisateur
-				LEFT JOIN utilisateurs.t_roles groupes ON groupes.id_role = cor_roles.id_role_groupe
-				LEFT JOIN utilisateurs.cor_app_privileges droits ON roles.id_role = droits.id_role OR groupes.id_role = droits.id_role
-				LEFT JOIN utilisateurs.t_tags ta ON droits.id_tag_action = ta.id_tag  
-				LEFT JOIN utilisateurs.t_tags tob ON droits.id_tag_object = tob.id_tag
-				LEFT JOIN utilisateurs.t_applications app ON droits.id_application = app.id_application
+			$sql = "SELECT roles.id_role, roles.identifiant, roles.nom_role, roles.prenom_role, roles.desc_role, roles.pass, roles.pass_plus, roles.email, array_to_json(array_agg(DISTINCT app.code_application)::character varying[]) as roles
+				FROM utilisateurs.v_roleslist_forall_applications roles
+				INNER JOIN utilisateurs.t_applications app ON roles.id_application = app.id_application
 				WHERE roles.id_role = :id
-				GROUP BY roles.id_role, roles.uuid_role, roles.identifiant, roles.nom_role, roles.prenom_role, roles.desc_role, roles.pass, roles.pass_plus, roles.email";
+				GROUP BY roles.id_role, roles.identifiant, roles.nom_role, roles.prenom_role, roles.desc_role, roles.pass, roles.pass_plus, roles.email";
 
 			$query = $this->_em->createNativeQuery($sql, $rsm);
 			$query->setParameter('id', $id);
