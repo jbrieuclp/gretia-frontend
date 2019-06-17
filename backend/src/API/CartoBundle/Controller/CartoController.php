a1c9ac98101bc79e2d9a212a437540eabf64d272
jbrieuclp <jbrieuclp@gmail.com>
2019-06-17 20:01:32 +0200

    ajout backend

---
+++ backend/src/API/CartoBundle/Controller/CartoController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace API\CartoBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use FOS\RestBundle\Controller\Annotations\Get;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+class CartoController extends FOSRestController implements ClassResourceInterface
+{
+    
+    /**
+    * @Rest\View(serializerGroups = {"projet"})
+    * @Security("has_role('METADATA')")
+    *
+    * @Rest\Get("/projets")
+    */
+    public function getProjetsAction()
+    {
+        $em = $this->getDoctrine()->getManager('gretiadb');
+        $cadres = $em->getRepository('APIMetadataBundle:CadreAcquisition')->findAll();
+        return $cadres;
+    }
+
+}
