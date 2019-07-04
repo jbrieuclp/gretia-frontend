463507d882e5f857646abe47452cd652e19b4aba
jbrieuclp <jbrieuclp@gmail.com>
2019-07-04 16:30:59 +0200

    config mode prod

--- frontend/src/app/shared/app.config.ts
+++ frontend/src/app/shared/app.config.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
 	appName : 'Gretia',
-	URL_API: 'http://rest-api.loc/app_dev.php',
-	URL_API_MT: 'http://rest-api.loc/app_dev.php/magic-taxref',
-	URL_API_CARTO: 'http://rest-api.loc/app_dev.php/carto',
+	URL_API: 'http://rest-api.loc',
+	URL_API_MT: 'http://rest-api.loc/magic-taxref',
+	URL_API_CARTO: 'http://rest-api.loc/carto',
 };
