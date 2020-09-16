542f269d145645c3cf4dfbe8260c6c5f37b75567
jbrieuclp <jbrieuclp@gmail.com>
2020-09-16 16:32:51 +0200

    config

--- frontend/src/app/shared/app.config.ts
+++ frontend/src/app/shared/app.config.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
 	appName : 'Gretia',
-	URL_API: 'http://rest-api.loc/app_dev.php',
+	URL_API: 'http://api.gretia.org',
 	URL_API_MT: 'http://api.gretia.org/magic-taxref',
-	URL_API_CARTO: 'http://rest-api.loc/app_dev.php/carto',
+	URL_API_CARTO: 'http://api.gretia.org/carto',
 	URL_PROJET: 'http://api.gretia.org/etude',
 	URL_API_IMPORT: 'http://api.gretia.org/import'
 };
